c6536783a795b9365c1f5c6515cc3da28a60d2f0
Daniil Golandzia <solovevdaniil01@gmail.com>
2022-11-14 03:17:46 +0300

    Добавьте файлы проекта.

---
+++ PracticeWork6/Model/Groups.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeWork6.Model
+{
+    public class Groups
+    {
+        public string Group { get; set; }
+        public int NumberOfGroup { get; set; }
+        public string NameOfGroup { get; set; }
+    }
+}
